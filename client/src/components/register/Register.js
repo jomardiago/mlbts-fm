10a0ecaedbfccc020be8d480a8600d1b239b52b2
jadiago <jadiago@cpi.com.ph>
2020-02-03 07:08:35 +0800

    Disable registration for now

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            Register Component
+        <div className="jumbotron">
+            <h1 class="display-4">We Apologize :(</h1>
+            <p>Registration is disabled for now. We're very sorry as we are still undergoing development.</p>
+            <p>We hope you will be patient and come back again as soon as we are done with the development.</p>
+            <p><strong>Thank you so much!</strong></p>
         </div>
     );
 };
