ad6593368bb8ed9df50fff697748b6dd6ef3ae25
jadiago <jadiago@cpi.com.ph>
2020-02-03 12:49:11 +0800

    Complete player progression

--- client/src/components/roster/PlayerProgression.js
+++ client/src/components/roster/PlayerProgression.js
@@ -1,7 +1,16 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const PlayerProgression = () => {
+const PlayerProgression = ({ roster, match }) => {
+    const { players } = roster;
+    const player = players.filter(player => player._id === match.params.id)[0];
+    player.progression.sort((a, b) => (a.year < b.year) ? 1 : -1);
+    const otherPositionPlayers = players.filter(otherPositionPlayer => 
+        otherPositionPlayer.primaryPosition === player.primaryPosition ||
+        otherPositionPlayer.secondaryPosition.includes(player.primaryPosition)
+    ).sort((a, b) => (a.overall < b.overall) ? 1 : -1);
+
     return (
         <Fragment>
             <div className="row">
@@ -10,7 +19,7 @@ const PlayerProgression = () => {
                         <div className="card-header">
                             <div className="row">
                                 <div className="col col-sm-10">
-                                    <h3>Aaron Judge</h3>
+                                    <h3>{player.firstName} {player.lastName}</h3>
                                 </div>
                                 <div className="col col-sm-2">
                                     <Link className="btn btn-primary ml-auto" to="/roster">Go Back</Link>
@@ -20,18 +29,18 @@ const PlayerProgression = () => {
                         <div className="card-body">
                             <div className="row m-auto">
                                 <div className="col col-sm-6">
-                                    <p className="card-text"><strong>Position: RF</strong></p>
+                                    <p className="card-text"><strong>Position: {player.primaryPosition}</strong></p>
                                 </div>
                                 <div className="col col-sm-6">
-                                    <p className="card-text"><strong>Secondary Position: OF</strong></p>
+                                    <p className="card-text"><strong>Secondary Position: {player.secondaryPosition}</strong></p>
                                 </div>
                             </div>
                             <div className="row m-auto">
                                 <div className="col col-sm-6">
-                                    <p className="card-text"><strong>Potential: A</strong></p>
+                                    <p className="card-text"><strong>Potential: {player.potential}</strong></p>
                                 </div>
                                 <div className="col col-sm-6">
-                                    <p className="card-text"><strong>Overall: 98</strong></p>
+                                    <p className="card-text"><strong>Overall: {player.overall}</strong></p>
                                 </div>
                             </div>
                         </div>
@@ -45,41 +54,49 @@ const PlayerProgression = () => {
                         <thead className="thead-dark">
                             <tr>
                                 <th>Year</th>
-                                <th>Class</th>
+                                <th>League</th>
                                 <th>Potential</th>
                                 <th>Overall</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>2019</td>
-                                <td>MLB</td>
-                                <td>A</td>
-                                <td>97</td>
-                            </tr>
+                            {
+                                player.progression.map(progress => (
+                                    <tr key={progress._id}>
+                                        <td>{progress.year}</td>
+                                        <td>{progress.league}</td>
+                                        <td>{progress.potential}</td>
+                                        <td>{progress.overall}</td>
+                                    </tr>
+                                ))
+                            }
                         </tbody>
                     </table>
                 </div>
                 <div className="col col-sm-6">
-                    <h5 className="text-center">Roster - RF</h5>
+                    <h5 className="text-center">Roster - {player.primaryPosition}</h5>
                     <table className="table">
                         <thead className="thead-dark">
                             <tr>
                                 <th>First Name</th>
                                 <th>Last Name</th>
-                                <th>Class</th>
+                                <th>League</th>
                                 <th>Potential</th>
                                 <th>Overall</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Aaron</td>
-                                <td>Hicks</td>
-                                <td>MLB</td>
-                                <td>B</td>
-                                <td>87</td>
-                            </tr>
+                            {
+                                otherPositionPlayers.map(oPlayer => (
+                                    <tr key={oPlayer._id}>
+                                        <td>{oPlayer.firstName}</td>
+                                        <td>{oPlayer.lastName}</td>
+                                        <td>{oPlayer.league}</td>
+                                        <td>{oPlayer.potential}</td>
+                                        <td>{oPlayer.overall}</td>
+                                    </tr>
+                                ))
+                            }
                         </tbody>
                     </table>
                 </div>
@@ -88,4 +105,8 @@ const PlayerProgression = () => {
     );
 };
 
-export default PlayerProgression;
+const mapStateToProps = state => ({
+    roster: state.roster
+});
+
+export default connect(mapStateToProps)(PlayerProgression);
