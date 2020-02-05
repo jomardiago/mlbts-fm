34f1dc947640d9fb19728daa6db001fa52204334
jadiago <jadiago@cpi.com.ph>
2020-02-05 08:24:02 +0800

    Change the layout of the Add Player Form

--- client/src/components/roster/AddPlayer.js
+++ client/src/components/roster/AddPlayer.js
@@ -35,95 +35,103 @@ const AddPlayer = ({ createPlayer, history }) => {
                     <h1 className="large text-primary">Create Player Form</h1>
                     <p className="lead"><i className="fas fa-user"></i> Add a new player to your roster</p>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="firstName">First Name</label>
-                    <input 
-                        id="firstName"
-                        className="form-control"
-                        type="firstName"
-                        name="firstName"
-                        value={firstName}
-                        onChange={e => onChange(e)} 
-                    />
+                <div className="row">
+                    <div className="col form-group">
+                        <input 
+                            id="firstName"
+                            className="form-control"
+                            type="text"
+                            name="firstName"
+                            placeholder="Enter First Name"
+                            value={firstName}
+                            onChange={e => onChange(e)} 
+                        />
+                    </div>
+                    <div className="col form-group">
+                        <input 
+                            id="lastName"
+                            className="form-control"
+                            type="text"
+                            name="lastName"
+                            placeholder="Enter Last Name"
+                            value={lastName}
+                            onChange={e => onChange(e)} 
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="lastName">Last Name</label>
-                    <input 
-                        id="lastName"
-                        className="form-control"
-                        type="lastName"
-                        name="lastName"
-                        value={lastName}
-                        onChange={e => onChange(e)} 
-                    />
+                <div className="row">
+                    <div className="col form-group">
+                        <input 
+                            id="primaryPosition"
+                            className="form-control"
+                            type="text"
+                            name="primaryPosition"
+                            placeholder="Enter Primary Position"
+                            value={primaryPosition}
+                            onChange={e => onChange(e)} 
+                        />
+                    </div>
+                    <div className="col form-group">
+                        <input 
+                            id="secondaryPosition"
+                            className="form-control"
+                            type="text"
+                            name="secondaryPosition"
+                            placeholder="Enter Secondary Position"
+                            value={secondaryPosition}
+                            onChange={e => onChange(e)} 
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="primaryPosition">Primary Position</label>
-                    <input 
-                        id="primaryPosition"
-                        className="form-control"
-                        type="primaryPosition"
-                        name="primaryPosition"
-                        value={primaryPosition}
-                        onChange={e => onChange(e)} 
-                    />
+                <div className="row">
+                    <div className="col form-group">
+                        <input 
+                            id="year"
+                            className="form-control"
+                            type="number"
+                            name="year"
+                            placeholder="Enter Progress Year"
+                            value={year}
+                            onChange={e => onChange(e)} 
+                        />
+                    </div>
+                    <div className="col form-group">
+                        <input 
+                            id="league"
+                            className="form-control"
+                            type="text"
+                            name="league"
+                            placeholder="Enter Player's Current League"
+                            value={league}
+                            onChange={e => onChange(e)} 
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="secondaryPosition">Secondary Position</label>
-                    <input 
-                        id="secondaryPosition"
-                        className="form-control"
-                        type="secondaryPosition"
-                        name="secondaryPosition"
-                        value={secondaryPosition}
-                        onChange={e => onChange(e)} 
-                    />
+                <div className="row">
+                    <div className="col form-group">
+                        <input 
+                            id="overall"
+                            className="form-control"
+                            type="number"
+                            name="overall"
+                            placeholder="Enter Overall"
+                            value={overall}
+                            onChange={e => onChange(e)} 
+                        />
+                    </div>
+                    <div className="col form-group">
+                        <input 
+                            id="potential"
+                            className="form-control"
+                            type="potential"
+                            name="potential"
+                            placeholder="Enter Potential"
+                            value={potential}
+                            onChange={e => onChange(e)} 
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="year">Roster Year</label>
-                    <input 
-                        id="year"
-                        className="form-control"
-                        type="year"
-                        name="year"
-                        value={year}
-                        onChange={e => onChange(e)} 
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="league">League</label>
-                    <input 
-                        id="league"
-                        className="form-control"
-                        type="league"
-                        name="league"
-                        value={league}
-                        onChange={e => onChange(e)} 
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="potential">Potential</label>
-                    <input 
-                        id="potential"
-                        className="form-control"
-                        type="potential"
-                        name="potential"
-                        value={potential}
-                        onChange={e => onChange(e)} 
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="overall">Overall</label>
-                    <input 
-                        id="overall"
-                        className="form-control"
-                        type="overall"
-                        name="overall"
-                        value={overall}
-                        onChange={e => onChange(e)} 
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary btn-lg">Submit</button>
+                <button type="submit" className="btn btn-primary">Add New Player</button>
             </form>
         </Fragment>
     );
